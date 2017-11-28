045c30237623dc1be31bcca9e11b56b754d95edd
Jesse <jesse.holwell@au.ey.com>
2017-11-28 16:17:48 +1100

    working with filecollection class -hacky arrays though

--- AppAudit/Model.cs
+++ AppAudit/Model.cs
@@ -10,30 +10,35 @@ namespace AppAudit
         [System.Xml.Serialization.XmlElement("Regex")]
         public string Regex { get; set; }
 
-        //File to search
-        [System.Xml.Serialization.XmlElement("Location")]
-        public string Location { get; set; }
-
         //Human-readable description
-        [System.Xml.Serialization.XmlElement("Description")]
+        [System.Xml.Serialization.XmlAttribute("Description")]
         public string Description { get; set; }
 
-        //Low medium high
-        //Priority
-
-        //Human-readable description
-        //Description
-
+        //Low medium high priority
         //Suggested fix or implementation?
-
+        //A link to the fix
     }
 
     [Serializable()]
-    [System.Xml.Serialization.XmlRoot("RuleCollection")]
-    public class RuleCollection
+    public class AuditFile
     {
+        //File name
+        [System.Xml.Serialization.XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        //List of rules for file
         [XmlArray("Rules")]
         [XmlArrayItem("Rule", typeof(Rule))]
         public Rule[] Rules { get; set; }
+    }
+
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("FileCollection")]
+    public class FileCollection
+    {
+        //List of files to analyze
+        [XmlArray("Files")]
+        [XmlArrayItem("AuditFile", typeof(AuditFile))]
+        public AuditFile[] Files { get; set; }
     }
 }
--- AppAudit/Program.cs
+++ AppAudit/Program.cs
@@ -1,108 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Xml;
+﻿using System.IO;
 using System.Xml.Serialization;
 
 namespace AppAudit
 {
     class Program
     {
-        public static RuleCollection AppRules { get; set; }
+        public static FileCollection AppRules { get; set; }
 
         static void Main(string[] args)
         {
             //parse the xml file - async?
             AppRules = GetRulesFromXml();
 
-            var proceed = true;
+        //    var proceed = true;
 
-            while (proceed)
-            {
+        //    while (proceed)
+        //    {
 
-                //ask for the project directory - async?
-                Console.WriteLine("Enter app location > ");
-                var appLocation = Console.ReadLine();
+        //        //ask for the project directory - async?
+        //        Console.WriteLine("Enter app location > ");
+        //        var appLocation = Console.ReadLine();
 
-                //HACK: 
-                appLocation = @"C:\Users\holweje\Source\Repos\Divestment\Divestment";
+        //        //HACK: 
+        //        appLocation = @"C:\Users\holweje\Source\Repos\Divestment\Divestment";
 
-                //search this location for any relevant files
-                //identified:
-                //web.configs
-                var files = new List<string>()
-                {
-                    "web.config",
-                    "web.staging.config",
-                    "web.release.config",
+        //        //search this location for any relevant files
+        //        //identified:
+        //        //web.configs
+        //        var files = new List<string>()
+        //        {
+        //            "web.config",
+        //            "web.staging.config",
+        //            "web.release.config",
 
-                    "global.asax",
-                    "startup.cs",
-                    @"Views\Shared\_Layout.cs"
-                };
+        //            "global.asax",
+        //            "startup.cs",
+        //            @"Views\Shared\_Layout.cs"
+        //        };
 
-                var locatedFiles = Directory.GetFiles(appLocation);
-                //app_start
-                //...
+        //        var locatedFiles = Directory.GetFiles(appLocation);
+        //        //app_start
+        //        //...
 
 
-                //TODO: better file identification logic?
-                Console.WriteLine("Files found:");
-                foreach (var path in locatedFiles)
-                    Console.WriteLine(path);
+        //        //TODO: better file identification logic?
+        //        Console.WriteLine("Files found:");
+        //        foreach (var path in locatedFiles)
+        //            Console.WriteLine(path);
 
-                foreach (var path in locatedFiles)
-                    foreach (var config in files)
-                        if (path.ToLower().Contains(config))
-                            AnalyzeFile(path, config);
+        //        foreach (var path in locatedFiles)
+        //            foreach (var config in files)
+        //                if (path.ToLower().Contains(config))
+        //                    AnalyzeFile(path, config);
 
-                //TODO: more graceful loop or exit
-                Console.WriteLine("Exit? (X))");
-                if (Console.ReadLine() == "X")
-                    proceed = false;
+        //        //TODO: more graceful loop or exit
+        //        Console.WriteLine("Exit? (X))");
+        //        if (Console.ReadLine() == "X")
+        //            proceed = false;
 
-            }
+        //    }
+        //}
+
+        //public static void AnalyzeFile(string path, string file)
+        //{
+        //    var lines = File.ReadAllLines(path);
+
+        //    var ruleScope = AppRules.Rules.Where(x => x.Location == file);
+        //    if (ruleScope.Any())
+        //    {
+        //        Console.WriteLine("\nAnalyzing: " + path);
+        //        foreach (var rule in ruleScope)
+        //        {
+        //            var matched = false;
+
+        //            foreach (var line in lines)
+        //                if (Regex.IsMatch(line, rule.Regex))
+        //                    matched = true;
+
+        //            Console.BackgroundColor = (matched) ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
+        //            Console.ForegroundColor = (matched) ? ConsoleColor.Green : ConsoleColor.Red;
+
+        //            Console.WriteLine(rule.Description + ":\t" + matched);
+
+        //            Console.BackgroundColor = ConsoleColor.Black;
+        //            Console.ForegroundColor = ConsoleColor.White;
+        //        }
+        //    }
+
         }
 
-        public static void AnalyzeFile(string path, string file)
-        {
-            var lines = File.ReadAllLines(path);
-
-            var ruleScope = AppRules.Rules.Where(x => x.Location == file);
-            if (ruleScope.Any())
-            {
-                Console.WriteLine("\nAnalyzing: " + path);
-                foreach (var rule in ruleScope)
-                {
-                    var matched = false;
-
-                    foreach (var line in lines)
-                        if (Regex.IsMatch(line, rule.Regex))
-                            matched = true;
-
-                    Console.BackgroundColor = (matched) ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
-                    Console.ForegroundColor = (matched) ? ConsoleColor.Green : ConsoleColor.Red;
-
-                    Console.WriteLine(rule.Description + ":\t" + matched);
-
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-            }
-        }
-
-        public static RuleCollection GetRulesFromXml()
+        public static FileCollection GetRulesFromXml()
         {
             string path = "BestPractices.xml";
 
-            RuleCollection rules = null;
-            XmlSerializer serializer = new XmlSerializer(typeof(RuleCollection));
+            FileCollection rules = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(FileCollection));
             StreamReader reader = new StreamReader(path);
-            rules = (RuleCollection)serializer.Deserialize(reader);
+            rules = (FileCollection)serializer.Deserialize(reader);
             reader.Close();
             return rules;
         }
