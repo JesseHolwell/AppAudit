98f95c93ddf9560e79034ab0b95a9f99e5a7470e
Jesse <jesse.holwell@au.ey.com>
2017-11-28 11:54:29 +1100

    Add project files.

---
+++ AppAudit/Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace AppAudit
+{
+    [Serializable()]
+    public class Rule
+    {
+        //String to match
+        [System.Xml.Serialization.XmlElement("Regex")]
+        public string Regex { get; set; }
+
+        //File to search
+        [System.Xml.Serialization.XmlElement("Location")]
+        public string Location { get; set; }
+
+        //Low medium high
+        //Priority
+
+        //Human-readable description
+        //Description
+
+        //Suggested fix or implementation?
+
+    }
+
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("RuleCollection")]
+    public class RuleCollection
+    {
+        [XmlArray("Rules")]
+        [XmlArrayItem("Rule", typeof(Rule))]
+        public Rule[] Rules { get; set; }
+    }
+}
---
+++ AppAudit/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace AppAudit
+{
+    class Program
+    {
+        public static RuleCollection AppRules {get;set;}
+
+        static void Main(string[] args)
+        {
+            //parse the xml file - async?
+            AppRules = GetRulesFromXml();
+            
+            //ask for the project directory - async?
+            Console.WriteLine("Enter app location > ");
+            var appLocation = Console.ReadLine();
+
+            //search this location for any relevant files
+            //identified:
+            //web.configs
+            //app_start
+            //...
+
+            foreach (var path in Directory.GetFiles(appLocation))
+            {
+                //TODO: better file identification logic?
+                //Console.WriteLine(path); // full path
+                //Console.WriteLine(System.IO.Path.GetFileName(path)); // file name
+                if (path.ToLower().Contains("web.config")) 
+                    DebugAnalysis(path);
+            }
+
+            //TODO: graceful loop or exit
+            Console.ReadLine();
+
+        }
+
+        public static void DebugAnalysis(string path)
+        {
+            var file = File.ReadAllLines(path);
+
+            foreach (var rule in AppRules.Rules)
+            {
+                var matched = false;
+
+                //var regex = new Regex("woff2");
+                //foreach (var line in file)
+                //    if (Regex.IsMatch(line, "woff2"))
+                //        matched = true;
+
+                //if (matched == false)
+                //    Console.WriteLine("Woff is missing");
+                //else
+                //    Console.WriteLine("Woff is present");
+            }
+
+        }
+
+        public static RuleCollection GetRulesFromXml()
+        {
+            string path = "BestPractices.xml";
+
+            RuleCollection rules = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(RuleCollection));
+            StreamReader reader = new StreamReader(path);
+            rules = (RuleCollection)serializer.Deserialize(reader);
+            reader.Close();
+            return rules;
+        }
+    }
+
+
+}
